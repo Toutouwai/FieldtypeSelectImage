835ebfb0550d7454ff0cfce8a2dac0910cacd8ba
Robin Sallis <Toutouwai@users.noreply.github.com>
2018-12-19 17:02:07 +1300

    v0.2.0
    Integrates a slightly modified version of Image Picker to allow visual selection of image.

--- InputfieldSelectImage.js
+++ InputfieldSelectImage.js
@@ -1,24 +1,3 @@
 $(function(){
-
-	var $select = $('.InputfieldSelectImage select');
-
-    // Add thumbnail when page loads
-	$select.each(function() {
-        appendThumb($(this));
-    });
-
-    // Add thumbnail when select changes
-	$select.on('change', function() {
-        appendThumb($(this));
-    });
-
+	$('.InputfieldSelectImage select').imagepicker({show_label: true});
 });
-
-function appendThumb(element){
-    var img_url = element.find(':selected').data("thumb");
-    var thumb_box = element.next(".selected-thumb");
-    thumb_box.empty();
-    if(img_url) {
-        thumb_box.append("<img src='" + img_url + "'>");
-    }
-}
